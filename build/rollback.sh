549f470d1448ed566bc80e2406e737d7965c1c47
Francois Perron <fperron@gmail.com>
2015-04-29 08:49:59 -0400

    with npm rollback:test|prod -- tag-name that rebuild env at that tag

--- build/rollback.sh
+++ build/rollback.sh
@@ -5,4 +5,6 @@ tag=$2
 
 ssh root@les-epicuriens-du-logiciel.ca bash -c "'
 git --work-tree=/root/les-epicuriens-du-logiciel-${env} --git-dir=/root/les-epicuriens-du-logiciel-${env}-repo checkout tags/${tag}
+cd /root/les-epicuriens-du-logiciel-${env}
+npm run build
 '"
